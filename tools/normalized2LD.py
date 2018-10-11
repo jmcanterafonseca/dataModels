a54269b53d509160668f8f3c6435a20ae46927a0
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-10-11 13:33:09 +0200

    Autopep 8

--- tools/normalized2LD.py
+++ tools/normalized2LD.py
@@ -34,11 +34,11 @@ def ld_id(entity_id, entity_type):
         d = parse(entity_id, rule='URI')
         print(d)
         if d['authority'] == None:
-             out = ngsild_uri(entity_type, entity_id)
+            out = ngsild_uri(entity_type, entity_id)
     except ValueError:
         print 'val error'
         out = ngsild_uri(entity_type, entity_id)
-    
+
     return out
 
 
@@ -53,43 +53,43 @@ def ld_object(attribute_name, entity_id):
             entity_type = attribute_name[3:]
 
         out = ngsild_uri(entity_type, entity_id)
-    
+
     return out
 
 
 # Prints the JSON string but with the proper key order
 def print_json_string(entity):
     print entity['id']
-    
+
     entity_only_id = {
         'id': entity['id'],
     }
-    
+
     out = json.dumps(entity_only_id, indent=4)[:-2]
-    
+
     entity_only_type = {
         'type': entity['type'],
     }
-    
+
     # with -2 it is removed the new line char as well
     out += "," + json.dumps(entity_only_type, indent=4)[1:-2]
 
     entity_cloned = deepcopy(entity)
-    
+
     del entity_cloned['id']
     del entity_cloned['type']
     del entity_cloned['@context']
-    
+
     # Now the rest of the entity without the '@context
-    out += "," + json.dumps(entity_cloned,indent=4)[1:-2] 
-    
+    out += "," + json.dumps(entity_cloned, indent=4)[1:-2]
+
     # Last go the '@context'
     only_ld_context = {
         '@context': entity['@context']
     }
-    
-    out += "," + json.dumps(only_ld_context, indent=4)[1:] 
-    
+
+    out += "," + json.dumps(only_ld_context, indent=4)[1:]
+
     return out
 
 
@@ -127,13 +127,13 @@ def normalized_2_LD(entity):
                 '@type': 'DateTime',
                 '@value': attr['value']
             }
-        
+
         if 'type' in attr and attr['type'] == 'PostalAddress':
             ld_attr['value']['type'] = 'PostalAddress'
 
         if 'metadata' in attr:
             metadata = attr['metadata']
-            
+
             for mkey in metadata:
                 if mkey == 'timestamp':
                     ld_attr['observedAt'] = metadata[mey]['value']
